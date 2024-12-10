333ea72855ad76885443cbf303df7cebda4666fd
Gabriel Locia <locciaa1@gmail.com>
2024-12-10 17:35:12 -0600

    Eliminar modelo productos en routes.js

--- routes.js
+++ routes.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const routes = express.Router();
-const products = require("./models/productos");
 const productsController = require("./controllers/Products/products")
 
 
 routes.get("/", async (req, res) => {
   const product = await ProductosModel.findById(id);
-  res.status(200).send({ response: "OK" });
+  res.status(200).send({ response: "OK", product });
 });
 
 routes.get("/products", (req, res) => productsController.getAllProdcuts(res))
 routes.post("/products/", (req, res) => productsController.createProduct(req, res))
 routes.put("/products/:id", (req, res) => productsController.updateProduct(req, res))
-routes.delete("/products/:id", (req, res) => productsController.updateProduct(req, res))
+routes.delete("/products/:id", (req, res) => productsController.deleteProduct(req, res))
 
 module.exports = routes;
